9b1b3f18e2a23e198bce0dfa0de0ede99be3a0ad
vladskvortsov <vladskvortsov96@gmail.com>
2022-10-03 19:43:43 +0300

    swarm added, db fix needed

--- Dockerfile
+++ Dockerfile
@@ -7,11 +7,10 @@ COPY *.sh /home/
 
 RUN bash drupal_install_nginx_no_db.sh
 
-CMD ["php8.1-fpm", "-D"]
-
 CMD ["nginx", "-g", "daemon off;"]
 
+#"php-fpm", 
 
 
+#RUN service php8.1-fpm start &&  service nginx start
 
-#RUN service nginx start && service php8.1-fpm start
--- web_install.sh
+++ web_install.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-sudo echo ' 
+sudo echo '
 network:
   ethernets:
     enp0s3:
@@ -19,11 +19,7 @@ sudo ssh osboxes@192.168.56.4
 sudo apt-get update -y
 sudo apt-get install -y ansible
 
-sudo apt-get install -y ansible-core
 
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3 get-pip.py --user
-python3 -m pip install --user ansible
 
 
 git clone https://github.com/vladskvortsov/ansible-drupal-deploy.git
@@ -32,3 +28,12 @@ sudo ansible-playbook playbook.yml -i ~/ansible-drupal-deploy/hosts.ini -u root
 
 #ssh -p 2000 osboxes@192.168.88.223
 #ssh -p 2020 osboxes@192.168.88.223
+
+
+
+
+#sudo docker swarm init
+#sudo docker network create --driver=overlay --attachable drupal-overlay-net
+#sudo docker run -d -p 80:80 --network drupal-overlay-net --link db:db --name drupal2 vladskvortsov/drupal_nginx_no_db:latest
+
+#sudo docker run -e MYSQL_ROOT_PASSWORD=root -dit --name db --network drupal-overlay-net mysql:8.0
